4f5f6e7c9aa5890bd43d82bd28ce4e4800e81206
sushant seth <96843481+sushantseth@users.noreply.github.com>
2023-04-18 16:19:38 +0530

    Add files via upload

---
+++ streams.js
@@ -0,0 +1,22 @@
+const server = require('http').createServer()
+const fs = require('fs')
+
+server.on('request',(req,res)=>{
+    const readable = fs.createReadStream('./read-file.txt')
+
+    // process 1
+    // readable.on('data', chunk => {
+    //     res.write(chunk)
+    // })
+    // readable.on('end', ()=> {
+    //     res.end()
+    // })
+
+    //process 2
+    //pipe is used to handle the problem of backpressure by controlling the speed of data coming in and the speed of data coming out
+    readable.pipe(res)
+})
+
+server.listen('8080','127.0.0.1',()=>{
+    console.log("listening...")
+})
